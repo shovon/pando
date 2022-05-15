f62ef12dc94324231291b7247e2538cac12741ce
Sal Rahman <salehen.rahman@gmail.com>
2022-05-14 23:58:54 -0700

    Some useless change

--- tree/main.go
+++ tree/main.go
@@ -156,11 +156,11 @@ func handleTree(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Create the challenge
-	challenge, err := createChallenge()
-	if err != nil {
-		log.Error().Err(err)
-		return
-	}
+	// challenge, err := ()
+	// if err != nil {
+	// 	log.Error().Err(err)
+	// 	return
+	// }
 
 	c.WriteJSON(challenge)
 
