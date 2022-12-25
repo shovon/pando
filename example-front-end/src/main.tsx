6a86d3587dbfaca4155c5432e495d4a41c0e7e76
Sal Rahman <salehen.rahman@gmail.com>
2022-12-24 16:09:44 -0800

    Got rooming function to work

--- example-front-end/src/main.tsx
+++ example-front-end/src/main.tsx
@@ -1,10 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
+import { GetRoom } from "./GetRoom";
 import "./index.css";
+import { Room } from "./Room";
+
+const router = createBrowserRouter([
+	{
+		path: "/room/:id",
+		element: <Room />,
+	},
+	{
+		path: "*",
+		element: <GetRoom />,
+	},
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</React.StrictMode>
 );
