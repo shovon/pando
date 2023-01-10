2004f35b6dfa3369aed06c92dd85b0aee76b064d
Sal Rahman <salehen.rahman@gmail.com>
2023-01-09 22:00:22 -0800

    More work on getting rooms to work

--- example/frontend/src/main.tsx
+++ example/frontend/src/main.tsx
@@ -4,9 +4,6 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { GetRoom } from "./components/GetRoom";
 import "./index.css";
 import { RoomView } from "./components/Room";
-// import { generateKeys } from "@sparkscience/wskeyid-browser/src/utils";
-// import { Session } from "./session";
-// import { toAsyncIterable } from "@sparkscience/wskeyid-browser/src/pub-sub";
 
 const router = createBrowserRouter([
 	{
@@ -18,32 +15,6 @@ const router = createBrowserRouter([
 		element: <GetRoom />,
 	},
 ]);
-
-// Promise.resolve()
-// 	.then(async function () {
-// 		const keys = await generateKeys();
-// 		const session = await new Session(
-// 			"ws://localhost:8080/room/some_room",
-// 			keys
-// 		);
-
-// 		session.sessionStatusChangeEvents.addEventListener((status) => {
-// 			if (status.type === "CONNECTING") {
-// 				console.log(
-// 					"status type is %s and sub status is %s",
-// 					status.type,
-// 					status.status
-// 				);
-// 			} else {
-// 				console.log("Status type is %s", status.type);
-// 			}
-// 		});
-
-// 		for await (const event of toAsyncIterable(session.messageEvents)) {
-// 			console.log(event);
-// 		}
-// 	})
-// 	.catch(console.error);
 
 function App() {
 	return (
--- example/frontend/src/room.ts
+++ example/frontend/src/room.ts
@@ -1,5 +1,38 @@
+import { generateKeys } from "@sparkscience/wskeyid-browser/src/utils";
+import { Session } from "./session";
+import { toAsyncIterable } from "@sparkscience/wskeyid-browser/src/pub-sub";
+
 export class Room {
+	private video: MediaStream | null = null;
+	private audio: MediaStream | null = null;
+
 	constructor(roomId: string) {}
 
-	connect() {}
+	connect() {
+		Promise.resolve()
+			.then(async function () {
+				const keys = await generateKeys();
+				const session = await new Session(
+					"ws://localhost:8080/room/some_room",
+					keys
+				);
+
+				session.sessionStatusChangeEvents.addEventListener((status) => {
+					if (status.type === "CONNECTING") {
+						console.log(
+							"status type is %s and sub status is %s",
+							status.type,
+							status.status
+						);
+					} else {
+						console.log("Status type is %s", status.type);
+					}
+				});
+
+				for await (const event of toAsyncIterable(session.messageEvents)) {
+					console.log(event);
+				}
+			})
+			.catch(console.error);
+	}
 }
